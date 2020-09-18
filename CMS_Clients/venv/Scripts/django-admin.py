9675f0b3e7601ab920129d045078d3ad160ac645
trphipps <70498781+trphipps@users.noreply.github.com>
2020-09-17 22:32:27 -0500

    Uploaded activity1
    Uploaded activity 1 to repository

---
+++ CMS_Clients/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tadph\pycharmprojects\djangoproject\cms_clients\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
